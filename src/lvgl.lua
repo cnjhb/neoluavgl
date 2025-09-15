6dbc4f30849fc0e1c429bbb598add51459abb50c
cnjhb <hikariz@qq.com>
2025-09-15 14:50:22 +0800

    fix(lvgl.lua): optmize AlignToPara

--- src/lvgl.lua
+++ src/lvgl.lua
@@ -1337,8 +1337,8 @@ end
 --- @class AlignToPara
 --- @field type ObjAlignType
 --- @field base Object
---- @field x_ofs integer
---- @field y_ofs integer
+--- @field x_ofs? integer
+--- @field y_ofs? integer
 
 --- Style properties
 --- @class StyleProp
